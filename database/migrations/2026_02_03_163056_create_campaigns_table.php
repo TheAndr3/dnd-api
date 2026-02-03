f1ad5c304b5c797a428d2b8af2772f17c6857c18
André Vinícius Diz Freitas <114784247+TheAndr3@users.noreply.github.com>
2026-02-03 14:37:21 -0300

    fix: mistiping in invitation_code string

--- database/migrations/2026_02_03_163056_create_campaigns_table.php
+++ database/migrations/2026_02_03_163056_create_campaigns_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('campaigns', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('invitation-code')->unique();
+            $table->string('invitation_code')->unique();
             $table->foreignId('gm_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
